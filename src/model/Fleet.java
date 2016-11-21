98ec25e23661674d2f6537f2fd764f1754f1a7d1
User <User@BFZ7J32>
2016-11-21 17:28:46 +0100

    Added new class regarding the updated logic

---
+++ src/model/Fleet.java
@@ -0,0 +1,41 @@
+package model;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import utilities.SectionType;
+import utilities.Constants;
+
+
+public class Fleet {
+
+	private ArrayList<Aircraft> aircraftList = new ArrayList<>();
+
+	public Fleet() {
+	}
+
+	public Iterator<Aircraft> getIterator() {
+		return aircraftList.iterator();
+	}
+
+	public Aircraft findAircraft(int aircraftNo) {
+		for (Aircraft aircraft : aircraftList) {
+			if (aircraft.getAircraftID() == aircraftNo)
+				return aircraft;
+		}
+		return null;
+	}
+
+	public void addAircraft(ArrayList<Aircraft> aircraft) {
+
+		//TODO: update the body
+//		while (aircraft.size() < Constants.NO_OF_SEATS) {
+
+//			if (aircraft.size() < Constants.NO_OF_FIRST_CLASS_SEATS) {
+//				aircraftList.add(new Aircraft(aircraft.size(), Constants.PRICE_FIRST, SectionType.FIRST));
+//			} else {
+//				aircraftList.add(new Aircraft(aircraft.size(), Constants.PRICE_ECONOMY, SectionType.ECONOMY));
+//			}
+//		}
+
+	}
+}
